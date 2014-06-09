213844e956299ae32f5a3482496281789f33363e
Nick Chubarov <nick.chubarov@gmail.com>
2014-06-09 16:14:50 +0400

    Prevent cattr accessor dependency

--- lib/expose_query.rb
+++ lib/expose_query.rb
@@ -4,9 +4,6 @@ require 'expose_query/controller_dsl'
 require 'expose_query/active_record_query_strategy'
 require 'decent_exposure'
 
-ActiveSupport.on_load(:action_controller) do
-  include ExposeQuery::ControllerDsl
-end
 
 DecentExposure::ActiveRecordStrategy.class_eval do
   include ExposeQuery::ActiveRecordQueryStrategy
